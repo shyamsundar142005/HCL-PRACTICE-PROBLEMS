6791af35d621fb0f3a8963cbdad3aa425181c66f
shyamsundar142005 <168510161+shyamsundar142005@users.noreply.github.com>
2026-02-23 17:53:49 +0530

    Added python problems

--- problem 18.py
+++ problem 18.py
@@ -1,10 +1,8 @@
 d1 = {'apple':10, 'orange':20}
 d2 = {'orange':30, 'banana':40}
-
 for key, value in d2.items():
     if key in d1:
         d1[key] += value
     else:
         d1[key] = value
-
 print(d1)
